b81857ce2c65aa966fd73b8a569e14f3ab3921f1
Vova Lokotkov <lv14doubleg@gmail.com>
2023-03-23 22:52:43 +0500

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,5 @@ import { program } from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0');
-program.parse();
+program.option('-f, --format <type>', 'output format')
+  .parse(process.argv);
