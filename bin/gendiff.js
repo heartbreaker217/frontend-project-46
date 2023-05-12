90d005c54605f82d1d73c7cf7d57213b6bd499d9
heartbreaker217 <118683005+heartbreaker217@users.noreply.github.com>
2023-05-12 08:34:47 +0500

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,5 @@ program
   .version('1.0.0')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.opts())))
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.opts().format)))
   .parse(process.argv);
